5d7b59f1e5080e7993e495fd785d2247bfc0f4d6
darlson <davertiser@gmail.com>
2020-05-29 22:59:20 -0600

    update Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -15,6 +15,13 @@ export default class Form extends Component {
         const {name, value} = e.target
         this.setState({ [name]: value })
     }
+    clearFields(e) {
+        this.setState({
+            name: '',
+            price: '',
+            img: ''
+        })
+    }
 
     render () {
         return (
@@ -22,7 +29,7 @@ export default class Form extends Component {
                 <input name='name' placeholder='Name' onChange={e => this.handleChange(e)} />
                 <input name='price' placeholder='Price' onChange={e => this.handleChange(e)} />
                 <input name='img' placeholder='Image URL' onChange={e => this.handleChange(e)} />
-                <button>Cancel</button>
+                <button onClick={e => this.clearFields(e)} >Cancel</button>
                 <button>Add to Inventory</button>
             </div>
         )
